73ca7b423a525661b3cac45ecb144a21b6c316ae
giovanni.lela@link-me.it <giovanni.lela@gmail.com>
2017-04-24 10:32:04 +0200

    basic time tracking on google spreadsheet

--- app/renderer/task.js
+++ app/renderer/task.js
@@ -2,11 +2,21 @@
 
 const EventEmitter = require('events');
 
-const yerbamate = require('yerbamate');
-
+const P = require('bluebird');
 const TaskStatus = require('../common/task_status');
 const TaskTimer = require('../common/timer');
-
+const GoogleSpreadsheet = require('google-spreadsheet')
+const doc = new P.promisifyAll( new GoogleSpreadsheet('1EhHKPhvjeQhTpEEbGCqntS4InLI1QfyYhtx4Dn9eT3w'));
+const creds = require('../../client-secret.json');
+let ws = null;
+doc.useServiceAccountAuthAsync(creds)
+    .then(() =>
+        doc.getInfoAsync()
+    )
+    .then((info) => {
+        console.log(info);
+        ws = doc.worksheets[0];
+    });
 
 const TaskEvents = new EventEmitter();
 TaskTimer(TaskEvents);
@@ -22,38 +32,48 @@ class Task {
         this.finishTime = null;
         this.elapsedTime = null;
         this.onTimeUpdate = null;
+        this.currentRow = null;
     }
 
     run(stdout, done) {
         if (this.isRunning()) {
             throw new Error("Trying to run task without stopping it first");
         }
+
         this.status = TaskStatus.running;
         this.beginTime = Date.now();
         this.finishTime = null;
-        this.proc = yerbamate.run(this.command, this.path, {
-                stderr: stdout,
-                stdout: stdout
-            },
-            (code) => {
-                if (this.status !== TaskStatus.stopped) this.status = yerbamate.successCode(code) ? TaskStatus.ok : TaskStatus.error;
-                this.finishTime = Date.now();
-                this.updateElapsedTime();
-                TaskEvents.removeListener("time-update", this.onTimeUpdate);
-                done();
-            });
         this.onTimeUpdate = () => {
             this.updateElapsedTime();
         };
         TaskEvents.on("time-update", this.onTimeUpdate);
         this.updateElapsedTime();
+
+        console.log('STARTING');
+        doc.worksheets[0].setHeaderRow(
+            ['task', 'time'],
+            () => doc.worksheets[0].addRow({task: this.title, time:this.elapsedTime},
+                this.getCurrentRow.bind(this)
+            )
+        )
+        //doc.worksheets[0].getRows({'A': this.title, 'B': 0}, (a,b) => console.log(a,b))*/
+    }
+
+    getCurrentRow(){
+        doc.worksheets[0].getRows((a, b) => {
+
+            console.log(a,b)
+            this.currentRow = b.pop();
+            log(this.currentRow);
+        });
     }
 
     stop(cb) {
-        if (this.isRunning()) {
-            yerbamate.stop(this.proc, cb);
-        } else if (cb) cb();
+        this.currentRow.time = this.elapsedTime;
+        this.currentRow.save(cb);
+        this.elapsedTime = 0;
         this.status = TaskStatus.stopped;
+        this.onTimeUpdate = ()=>{}
     }
 
     isRunning() {
@@ -75,6 +95,7 @@ class Task {
         if (finishTime === null) finishTime = Date.now();
 
         this.elapsedTime = Math.trunc((finishTime - this.beginTime) / 1000);
+        console.log(this.elapsedTime)
     }
 }
 
